2b301c5e387ff86ad422baed60e844b6dbd523d9
prasanth r <112082713+rk-prasanth1@users.noreply.github.com>
2024-06-25 13:06:48 +0530

    Delete ec2.tf

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "dev" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  count         = var.instance_count
-
-  tags = {
-    Name = "webserver ${count.index}"
-  }
-}
